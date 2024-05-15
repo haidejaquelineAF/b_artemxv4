a55cd7bb7cc559000d27cae163f276a518e86a34
haidejaquelineAF <66649588+haidejaquelineAF@users.noreply.github.com>
2024-05-15 12:21:45 -0600

    Create Config.php

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const base_url ="http://localhost/b_artemxv4/";//Almacenar la ruta raíz del proyecto
+    const host = "localhost";   //Indicarle que nos vamos a conectar por medio de localhost
+    const user = "root";        //Indicarle el usuario
+    const pass = "";            //Para la clave
+    const db = "bd_bolsaartemx_prueba";       //Para indicar la base de datos
+    const charset = "charset-utf8"; //Indicar que codificación se va a usar
+?>
